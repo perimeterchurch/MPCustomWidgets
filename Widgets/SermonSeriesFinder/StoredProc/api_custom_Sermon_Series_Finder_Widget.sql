a2728bcdd967151cfc354f8760cdef29bab754c3
danab <danab@perimeter.org>
2024-04-05 13:07:00 -0400

    Adjusted MostRecentSermon Widget

--- Widgets/SermonSeriesFinder/StoredProc/api_custom_Sermon_Series_Finder_Widget.sql
+++ Widgets/SermonSeriesFinder/StoredProc/api_custom_Sermon_Series_Finder_Widget.sql
@@ -1,13 +1,10 @@
+USE [MinistryPlatform]
+GO
+/****** Object:  StoredProcedure [dbo].[api_custom_Sermon_Series_Finder_Widget]    Script Date: 4/5/2024 8:29:07 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[api_custom_Sermon_Series_Finder_Widget]') AND type in (N'P', N'PC'))
-BEGIN
-EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[api_custom_Sermon_Series_Finder_Widget] AS' 
-END
-GO
-
 -- =============================================
 -- Description:	This stored procedure returns sermons series data for the sermon series finder custom widget.
 -- Create date: 4/2/2024
@@ -24,11 +21,11 @@ AS
 BEGIN
 	
 	/* DATASET 1 */
-	SELECT S.Title AS Sermon_Title
+	SELECT TOP 1 S.Title AS Sermon_Title
 		, SS.Sermon_Series_ID
 		, SS.Title AS Series_Title
 		, CONVERT(nvarchar, S.Sermon_Date, 107) AS Sermon_Date
-		, C.First_Name + ' ' + C.Last_Name AS Full_Name
+		, C.Nickname + ' ' + C.Last_Name AS Full_Name
 		, S.Description
 		, (SELECT SL.Link_URL 
 			FROM Pocket_Platform_Sermon_Links SL 
@@ -38,11 +35,13 @@ BEGIN
 			FROM Pocket_Platform_Sermon_Links SL 
 			WHERE SL.Sermon_ID = S.Sermon_ID
 			AND SL.Link_Type_ID = 2) AS Audio_Link
+		, S.Banner_URL AS Photo_URL
 	FROM Pocket_Platform_Sermons S
 	INNER JOIN Pocket_Platform_Sermon_Series SS ON SS.Sermon_Series_ID = S.Series_ID
 	INNER JOIN Pocket_Platform_Speakers SP ON SP.Speaker_ID = S.Speaker_ID
 	INNER JOIN Contacts C ON C.Contact_ID = SP.Contact_ID
-	WHERE S.Sermon_ID = (SELECT MAX(Sermon_ID) FROM Pocket_Platform_Sermons)
+	INNER JOIN dp_Domains D On D.Domain_ID = C.Domain_ID
+	WHERE S.Sermon_Date = (SELECT MAX(Sermon_Date) FROM Pocket_Platform_Sermons)
 	AND SS.Sermon_Series_Type_ID = @Sermon_Series_Type
 	;
 
@@ -51,11 +50,13 @@ BEGIN
 		, SS.Title AS Series_Title
 		, CONVERT(nvarchar, MIN(S.sermon_date),107) AS [Start_Date]
 		, CONVERT(nvarchar, MAX(S.sermon_date),107) AS [End_Date]
-		, 'https://www.perimeter.org/images/thumbs/23452' AS Photo_URL
+		,'https://ministryplatform.perimeter.org/ministryplatformapi/Api.svc/rst/getfile?dn=' + CONVERT(Varchar(40),D.Domain_GUID) + '&fn=' + Convert(Varchar(40), F.Unique_Name) AS Photo_URL
 	FROM Pocket_Platform_Sermons S
 	INNER JOIN Pocket_Platform_Sermon_Series SS ON S.Series_ID = SS.Sermon_Series_ID
+	INNER JOIN dp_Domains D ON D.Domain_ID = SS.Domain_ID
+	LEFT JOIN dp_Files F ON F.Record_ID = SS.Sermon_Series_ID AND F.Page_ID = 632
 	WHERE (@Sermon_Series_Type IS NULL OR @Sermon_Series_Type = SS.Sermon_Series_Type_ID)
-	GROUP BY SS.Sermon_Series_ID, SS.Title
+	GROUP BY SS.Sermon_Series_ID, SS.Title, D.Domain_GUID, F.Unique_Name
 	ORDER BY MAX(S.Sermon_Date) DESC
 
 END
